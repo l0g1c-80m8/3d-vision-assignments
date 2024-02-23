81505e45509805077b4f4396829f71bdc9dfce24
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-02-22 23:01:23 -0800

    pass containerId to render and load each one separately

--- js/assignment1.js
+++ js/assignment1.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 const render = (containerId, asset) => {
-	const container = document.getElementById( 'container1' );
+	const container = document.getElementById(containerId);
 	container.style.position = 'relative';
 
 	let renderer, stats, gui;
@@ -14,25 +14,24 @@ const render = (containerId, asset) => {
 
 	function initScene() {
 		scene = new THREE.Scene();
-		scene.background = new THREE.Color( 0xffffff);
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight * 0.5), 0.1, 1000 );
+		scene.background = new THREE.Color(0xffffff);
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight * 0.5), 0.1, 1000);
 
 		renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight * 0.5 );
-		container.appendChild( renderer.domElement );
+		renderer.setSize(window.innerWidth, window.innerHeight * 0.5);
+		container.appendChild(renderer.domElement);
 
-		controls = new OrbitControls( camera, renderer.domElement );
+		controls = new OrbitControls(camera, renderer.domElement);
 		controls.minDistance = 2;
 		controls.maxDistance = 10;
-		controls.addEventListener( 'change', function() { renderer.render( scene, camera ); });
+		controls.addEventListener('change', function () { renderer.render(scene, camera); });
 
-		dirlight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-		dirlight.position.set( 0, 0, 1 );
-		scene.add( dirlight );
+		dirlight = new THREE.DirectionalLight(0xffffff, 0.5);
+		dirlight.position.set(0, 0, 1);
+		scene.add(dirlight);
 
-		ambientLight = new THREE.AmbientLight( 0x404040, 2 );
-		scene.add( ambientLight );
-
+		ambientLight = new THREE.AmbientLight(0x404040, 2);
+		scene.add(ambientLight);
 
 		// the loading of the object is asynchronous
 		let loader = new OBJLoader();
@@ -40,20 +39,20 @@ const render = (containerId, asset) => {
 			// resource URL
 			asset,
 			// called when resource is loaded
-			function ( object ) {
+			function (object) {
 				cube = object.children[0];
-				cube.material = new THREE.MeshPhongMaterial( { color: 0x999999 });
-				cube.position.set( 0, 0, 0 );
+				cube.material = new THREE.MeshPhongMaterial({ color: 0x999999 });
+				cube.position.set(0, 0, 0);
 				cube.name = asset;
-				scene.add( cube );
+				scene.add(cube);
 			},
 			// called when loading is in progresses
-			function ( xhr ) {
-				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+			function (xhr) {
+				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 			},
 			// called when loading has errors
-			function ( error ) {
-				console.log( 'An error happened' + error);
+			function (error) {
+				console.log('An error happened' + error);
 			}
 		);
 
@@ -62,49 +61,49 @@ const render = (containerId, asset) => {
 
 	function initSTATS() {
 		stats = new Stats();
-		stats.showPanel( 0 );
+		stats.showPanel(0);
 		stats.dom.style.position = 'absolute';
 		stats.dom.style.top = 0;
 		stats.dom.style.left = 0;
-		container.appendChild( stats.dom );
+		container.appendChild(stats.dom);
 	}
 
 	function initGUI() {
 		if (!isinitialized) {
 			gui = new GUI();
-			cube = scene.getObjectByName( asset );
-			gui.add( cube.position, 'x', -1, 1 );
-			gui.add( cube.position, 'y', -1, 1 );
-			gui.add( cube.position, 'z', -1, 1 );
+			cube = scene.getObjectByName(asset);
+			gui.add(cube.position, 'x', -1, 1);
+			gui.add(cube.position, 'y', -1, 1);
+			gui.add(cube.position, 'z', -1, 1);
 			gui.domElement.style.position = 'absolute';
 			gui.domElement.style.top = '0px';
 			gui.domElement.style.right = '0px';
-			container.appendChild( gui.domElement );
+			container.appendChild(gui.domElement);
 			isinitialized = true;
 		}
 	}
 
 	function animate() {
-		requestAnimationFrame( animate );
+		requestAnimationFrame(animate);
 
-		cube = scene.getObjectByName( asset );
+		cube = scene.getObjectByName(asset);
 		if (cube) {
 			cube.rotation.x += 0.01;
 			cube.rotation.y += 0.01;
 			initGUI(); // initialize the GUI after the object is loaded
 		}
 
-		renderer.render( scene, camera );
+		renderer.render(scene, camera);
 		stats.update();
 	}
 
 	function onWindowResize() {
 		camera.aspect = window.innerWidth / (window.innerHeight * 0.5);
 		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight * 0.5 );
+		renderer.setSize(window.innerWidth, window.innerHeight * 0.5);
 	};
 
-	window.addEventListener( 'resize', onWindowResize, false );
+	window.addEventListener('resize', onWindowResize, false);
 
 	initScene();
 	initSTATS();
