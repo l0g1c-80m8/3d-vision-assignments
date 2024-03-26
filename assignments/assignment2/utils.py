01cb55d11f39b72a43aed23bcd20297e5cf229d1
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-03-26 14:10:19 -0700

    fix the utils file

--- assignments/assignment2/utils.py
+++ assignments/assignment2/utils.py
@@ -1,8 +1,9 @@
-import numpy as np 
-import cv2 
+import numpy as np
+import cv2
 import pdb
 
-def serialize_keypoints(kp): 
+
+def serialize_keypoints(kp):
     """Serialize list of keypoint objects so it can be saved using pickle
     
     Args: 
@@ -12,13 +13,14 @@ def serialize_keypoints(kp):
     out: Serialized list of keypoint objects"""
 
     out = []
-    for kp_ in kp: 
+    for kp_ in kp:
         temp = (kp_.pt, kp_.size, kp_.angle, kp_.response, kp_.octave, kp_.class_id)
         out.append(temp)
 
     return out
 
-def deserialize_keypoints(kp): 
+
+def deserialize_keypoints(kp):
     """Deserialize list of keypoint objects so it can be converted back to
     native opencv's format.
     
@@ -30,12 +32,14 @@ def deserialize_keypoints(kp):
 
     out = []
     for point in kp:
-        temp = cv2.KeyPoint(x=point[0][0],y=point[0][1],size=point[1], angle=point[2], response=point[3], octave=point[4], class_id=point[5]) 
+        temp = cv2.KeyPoint(x=point[0][0], y=point[0][1], size=point[1], angle=point[2], response=point[3],
+                            octave=point[4], class_id=point[5])
         out.append(temp)
 
     return out
 
-def serialize_matches(matches): 
+
+def serialize_matches(matches):
     """Serializes dictionary of matches so it can be saved using pickle
     
     Args: 
@@ -45,12 +49,13 @@ def serialize_matches(matches):
     out: Serialized list of matches object"""
 
     out = []
-    for match in matches: 
-        matchTemp = (match.queryIdx, match.trainIdx, match.imgIdx, match.distance) 
+    for match in matches:
+        matchTemp = (match.queryIdx, match.trainIdx, match.imgIdx, match.distance)
         out.append(matchTemp)
     return out
 
-def deserialize_matches(matches): 
+
+def deserialize_matches(matches):
     """Deserialize dictionary of matches so it can be converted back to 
     native opencv's format. 
     
@@ -62,34 +67,36 @@ def deserialize_matches(matches):
 
     out = []
     for match in matches:
-        out.append(cv2.DMatch(match[0],match[1],match[2],match[3])) 
+        out.append(cv2.DMatch(match[0], match[1], match[2], match[3]))
     return out
 
-def pts2ply(pts,colors,filename='out.ply'): 
+
+def pts2ply(pts, colors, filename='out.ply'):
     """Saves an ndarray of 3D coordinates (in meshlab format)"""
 
-    with open(filename,'w') as f: 
+    with open(filename, 'w') as f:
         f.write('ply\n')
         f.write('format ascii 1.0\n')
         f.write('element vertex {}\n'.format(pts.shape[0]))
-        
+
         f.write('property float x\n')
         f.write('property float y\n')
         f.write('property float z\n')
-        
+
         f.write('property uchar red\n')
         f.write('property uchar green\n')
         f.write('property uchar blue\n')
-        
-        f.write('end_header\n')
-        
-        #pdb.set_trace()
-        colors = colors.astype(int)
-        for pt, cl in zip(pts,colors): 
-            f.write('{} {} {} {} {} {}\n'.format(pt[0],pt[1],pt[2],
-                                                cl[0],cl[1],cl[2]))
 
-def draw_correspondences(img, ptsTrue, ptsReproj, ax, drawOnly=50): 
+        f.write('end_header\n')
+
+        # pdb.set_trace()
+        colors = colors.astype(int)
+        for pt, cl in zip(pts, colors):
+            f.write('{} {} {} {} {} {}\n'.format(pt[0], pt[1], pt[2],
+                                                 cl[0], cl[1], cl[2]))
+
+
+def draw_correspondences(img, ptsTrue, ptsReproj, ax, drawOnly=50):
     """
     Draws correspondence between ground truth and reprojected feature point
 
@@ -102,7 +109,7 @@ def draw_correspondences(img, ptsTrue, ptsReproj, ax, drawOnly=50):
     ax: matplotlib axis object
     """
     ax.imshow(img)
-    
+
     # TODO: draw correspondence between ptsTrue and ptsReproj
 
     return ax
